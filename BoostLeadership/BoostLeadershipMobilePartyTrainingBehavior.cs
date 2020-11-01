c32bbe9570779e21d6f53459be1464b1a5c35aeb
David Mizrahi <david.mizrahi@atos.net>
2020-11-01 14:10:11 +0100

    rzqr

---
+++ BoostLeadership/BoostLeadershipMobilePartyTrainingBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace BoostLeadership
+{
+
+    internal class BoostLeadershipMobilePartyTrainingBehavior : CampaignBehaviorBase
+    {
+        public const float MoraleThreshold = 70f;
+        public override void RegisterEvents()
+        {
+            CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener((object)this, new Action<MobileParty>(this.DailyTickParty));
+        }
+
+        private void DailyTickParty(MobileParty mobileParty)
+        {
+            if (mobileParty.LeaderHero == null || mobileParty.Morale > 75.0 || mobileParty.Morale <= MoraleThreshold)
+                return;
+            mobileParty.LeaderHero?.HeroDeveloper.AddSkillXp(DefaultSkills.Leadership, (float)MathF.Round((float)(0.00999999977648258 * (double)mobileParty.MemberRoster.TotalManCount * ((double)mobileParty.Morale - (MoraleThreshold - 5)))), true, true);
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+
+        }
+    }
+}
--- BoostLeadership/BoostLeadershipSubModule.cs
+++ BoostLeadership/BoostLeadershipSubModule.cs
@@ -1,56 +1,48 @@
-﻿using HarmonyLib;
-using System;
-using System.Reflection;
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
 namespace BoostLeadership
 {
     public class BoostLeadershipSubModule : MBSubModuleBase
     {
+
+        public const float MoraleThreshold = 65f;
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
-            try
-            {
-                base.OnSubModuleLoad();
-                Harmony.DEBUG = true;
-                FileLog.Reset();
-                Harmony harmony1 = new Harmony("mod.bannerlord.boostleadership");
-                harmony1.PatchAll(Assembly.GetExecutingAssembly());
-            }
-            catch (Exception ex)
-            {
-                FileLog.Log($"Can't load BoostLeadership sub module : {ex.Message}");
-                //Handle exceptions
-            }
             
         }
 
-        //protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
-        //{
-        //    try
-        //    {
-        //        if (!(gameStarterObject is CampaignGameStarter))
-        //            return;
-        //        this.AddBehaviors(game, (CampaignGameStarter)gameStarterObject);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //    }
-        //}
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            try
+            {
+                if (!(gameStarterObject is CampaignGameStarter))
+                    return;
+                this.AddBehaviors(game, (CampaignGameStarter)gameStarterObject);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
 
-        //private void AddBehaviors(Game game, CampaignGameStarter gameInitializer)
-        //{
-        //    try
-        //    {
-        //        if (!(game.GameType is Campaign))
-        //            return;
-        //        gameInitializer.AddBehavior((CampaignBehaviorBase)new BoostLeadershipMobilePartyTrainingBehavior());
-        //        InformationManager.DisplayMessage(new InformationMessage($"{nameof(BoostLeadershipMobilePartyTrainingBehavior)} added"));
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //    }
-        //}
+
+
+        private void AddBehaviors(Game game, CampaignGameStarter gameInitializer)
+        {
+            try
+            {
+                if (!(game.GameType is Campaign))
+                    return;
+                
+                gameInitializer.AddBehavior(new BoostLeadershipMobilePartyTrainingBehavior());
+            }
+            catch (Exception ex)
+            {
+            }
+        }
     }
 }
