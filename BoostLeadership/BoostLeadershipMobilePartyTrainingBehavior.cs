3ea7c07251eb9cb7880e8a537c5d0c3325ab20cb
David Mizrahi <david.mizrahi@atos.net>
2021-05-13 18:36:38 +0200

    1.1.1

--- BoostLeadership/BoostLeadershipMobilePartyTrainingBehavior.cs
+++ BoostLeadership/BoostLeadershipMobilePartyTrainingBehavior.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterDevelopment.Managers;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 
 namespace BoostLeadership
 {
-
-    internal class BoostLeadershipMobilePartyTrainingBehavior : CampaignBehaviorBase
+    public class BoostLeadershipMobilePartyTrainingBehavior : CampaignBehaviorBase
     {
-        public const float MoraleThreshold = 70f;
+        public double MoraleThreshold = 65.0;
+
         public override void RegisterEvents()
         {
             CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener((object)this, new Action<MobileParty>(this.DailyTickParty));
         }
 
-        private void DailyTickParty(MobileParty mobileParty)
-        {
-            if (mobileParty.LeaderHero == null || mobileParty.Morale > 75.0 || mobileParty.Morale <= MoraleThreshold)
-                return;
-            mobileParty.LeaderHero?.HeroDeveloper.AddSkillXp(DefaultSkills.Leadership, (float)MathF.Round((float)(0.00999999977648258 * (double)mobileParty.MemberRoster.TotalManCount * ((double)mobileParty.Morale - (MoraleThreshold - 5)))), true, true);
-        }
-
         public override void SyncData(IDataStore dataStore)
         {
+        }
 
+        private void DailyTickParty(MobileParty mobileParty)
+        {
+            if (mobileParty.LeaderHero == null || (double)mobileParty.Morale > 75.0 || (double) mobileParty.Morale <= MoraleThreshold)
+                return;
+            mobileParty.LeaderHero?.HeroDeveloper.AddSkillXp(DefaultSkills.Leadership, (float)MathF.Round((float)(0.00999999977648258 * (double)mobileParty.MemberRoster.TotalManCount * ((double)mobileParty.Morale - (MoraleThreshold - 5)))), true, true);
         }
     }
 }
